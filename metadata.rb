f96706c15d3bdd6e32e74da65f9cfeacc5718231
Nick Pegg <nick@nickpegg.com>
2021-08-06 18:17:37 -0700

    bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'nick@nickpegg.com'
 license 'MIT'
 description 'Sets up a workstation the way I like it'
 long_description 'Sets up a workstation the way I like it'
-version '0.2.6'
+version '0.2.7'
 
 depends 'apt'
 depends 'docker', '>=4.12.0'
