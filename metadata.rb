17d89a2e8ec95c46b0587c8be204617a876124fc
Nick Pegg <nick@nickpegg.com>
2022-05-07 18:09:47 -0700

    WIP - u22 support - blocked on VirtualBox providing packages

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,10 @@ maintainer 'Nick Pegg'
 maintainer_email 'nick@nickpegg.com'
 license 'MIT'
 description 'Sets up a workstation the way I like it'
-version '0.2.9'
+version '0.2.10'
 
 depends 'docker', '>=4.12.0'
 
 supports 'arch'
 supports 'ubuntu', '= 20.04'
+supports 'ubuntu', '= 22.04'
