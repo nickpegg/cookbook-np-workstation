50a6ede100841a11a5e0082425b358282f64d23e
Nick Pegg <nick@nickpegg.com>
2022-05-11 16:39:40 -0700

    Add GitHub CLI (#12)
    add github cli

--- recipes/default.rb
+++ recipes/default.rb
@@ -39,9 +39,28 @@ base_packages.each do |pkg|
   package pkg
 end
 
+########################################
+# GitHub CLI
+if platform? 'arch'
+  package 'github-cli'
+elsif platform_family? 'debian'
+  apt_repository 'github-cli' do
+    uri 'https://cli.github.com/packages'
+    key 'https://cli.github.com/packages/githubcli-archive-keyring.gpg'
+    distribution 'stable'
+    components ['main']
+  end
+
+  package 'gh'
+end
+
+########################################
+# Virtualbox and Vagrant
 include_recipe 'np-workstation::virtualbox'
 package 'vagrant'
 
+########################################
+# Docker
 if platform?('ubuntu')
   docker_installation 'default'
 else
